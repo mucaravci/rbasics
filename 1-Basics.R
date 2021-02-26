eddcaa187d4f53a2ca5ea3824ff79c22767fb650
Awppenheimer <mucar@avci.com>
2021-02-26 15:22:21 +0300

    Create 1-Basics.R

---
+++ 1-Basics.R
@@ -0,0 +1,36 @@
+#R TUTORIAL
+
+#printing
+
+"Hello World!"
+#or
+print("Hello World")
+
+for (x in 1:5) {
+  print(x)
+}
+
+#variables
+name="John Doe"
+age="40"
+print(name)
+#John Doe
+print(age)
+#40
+paste(name, "is", age)
+#John Doe is 40
+
+num1=5
+num2=4
+print(num1+num2)
+#9
+
+var1=var2=var3="Mehmet"
+#All three variables will output Mehmet
+
+#VARIABLE NAME SUGGESTIONS
+  #Variables CAN have letters, digits, periods etc.
+  #Variables CANNOT start with numbers or underscore
+  #Variables are case sensitive
+  #Reserved words such as TRUE, FALSE, NULL and so on
+  #CANNOT be used as variables.
